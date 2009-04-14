b7d5af8d27f6c1b285b0ba539f0a4743243a39c7
Nikhil Handigol <nikhilh@hh-vm.(none)>
2009-04-14 02:55:48 -0700

    missed file

--- cli/.cli.d
+++ cli/.cli.d
@@ -1,2 +1,3 @@
 cli.o: cli.c cli.h cli_help.h cli_network.h helper.h socket_helper.h \
-  search_state.h ../sr_base_internal.h cli_stubs.h
+  search_state.h ../sr_base_internal.h ../routingTable.h ../sr_vns.h \
+  ../sr_base_internal.h ../sr_integration.h cli_stubs.h
